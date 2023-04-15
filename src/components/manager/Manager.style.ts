c8a3763f0932a2abc5b36bbd91a2ef48829c8772
bae0203 <gmbae06@dgsw.hs.kr>
2023-04-15 22:17:46 +0900

    ADD :: Style

---
+++ src/components/manager/Manager.style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ManagerMainContainer = styled.nav`
+  position: absolute;
+  width: 100%;
+  height: 90%;
+  background-color: #f2f4f6;
+  padding-top: 2%;
+  display: flex;
+`;
+
+export const ContentsBox = styled.nav`
+  width: 80%;
+  height: 100%;
+  background-color: #fff;
+  border-top-right-radius: 4rem;
+`;
---
+++ src/components/manager/sidebar/Sidebar.style.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import Logo from "../../../asset/CNSLogo.svg";
+import Image from "next/image";
+
+export const MainContainer = styled.nav`
+  width: 20%;
+  height: 100%;
+  background-color: #fafafa;
+  box-shadow: inset -0.25rem -0rem 1rem rgba(0, 0, 0, 0.1);
+`;
+
+export const TitleContainer = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  margin: 0rem auto;
+  padding: 2rem 0rem;
+`;
+
+export const TitleImage = styled(Image)`
+  background-image: url(${Logo});
+  background-size: cover;
+  width: 55px;
+  height: 55px;
+  border-radius: 50%;
+  margin-right: 0.8rem;
+  box-shadow: 0.2rem 0.2rem 0.5rem rgba(0, 0, 0, 0.7);
+`;
+
+export const TitleContext = styled.p`
+  font-size: 1.25rem;
+  color: #394050;
+  font-weight: bold;
+  text-shadow: 0rem 0.2rem 0.3rem rgba(0, 0, 0, 0.4);
+`;
+
+export const InquiryBox = styled.nav`
+  bottom: 0;
+  width: 100%;
+  height: 2rem;
+  background-color: blue;
+`;
