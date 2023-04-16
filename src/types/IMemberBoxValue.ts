db57836f6092615679a641065e85a78b0304be6f
bae0203 <gmbae06@dgsw.hs.kr>
2023-04-16 19:48:20 +0900

    ADD :: Member manage Box Value

---
+++ src/types/IMemberBoxValue.ts
@@ -0,0 +1,11 @@
+export interface IMemberBoxValue {
+  title: string;
+  member: IMemberValue[];
+}
+
+interface IMemberValue {
+  name: string;
+  schoolNumber: string;
+  phoneNumber: string;
+  introduce: string;
+}
