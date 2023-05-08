308b5f013fccad4838cfc8d49ee6a552cebfbfca
bae0203 <gmbae06@dgsw.hs.kr>
2023-05-08 11:20:34 +0900

    💩 [ManagerPage] 코드 테스트

--- src/components/manager/Manager.tsx
+++ src/components/manager/Manager.tsx
@@ -2,22 +2,70 @@ import React from "react";
 import Title from "./contentsBox/Title";
 import * as S from "./Manager.style";
 import MemederBox from "./member/MemederBox";
+import { IMemberBoxValue } from "@/types/IMemberBoxValue";
 import Sidebar from "./sidebar/Sidebar";
 
 const Manager = () => {
+  const MemberBox1: IMemberBoxValue = {
+    title: "📩 지원 접수",
+    member: [
+      {
+        name: "배경민",
+        schoolNumber: "2307",
+        phoneNumber: "010-6207-7445",
+        introduce: "열심히 하겠습니다.",
+      },
+      {
+        name: "조수현",
+        schoolNumber: "2301",
+        phoneNumber: "010-1234-4567",
+        introduce: "열심히 하겠습니다.",
+      },
+      {
+        name: "최아영",
+        schoolNumber: "2403",
+        phoneNumber: "010-1234-5678",
+        introduce: "열심히 하겠습니다.",
+      },
+    ],
+  };
+  const MemberBox2: IMemberBoxValue = {
+    title: "👩‍💻 면접",
+    member: [
+      {
+        name: "조수현",
+        schoolNumber: "2301",
+        phoneNumber: "010-1234-4567",
+        introduce: "열심히 하겠습니다.",
+      },
+      {
+        name: "최아영",
+        schoolNumber: "2403",
+        phoneNumber: "010-1234-5678",
+        introduce: "열심히 하겠습니다.",
+      },
+    ],
+  };
+  const MemberBox3: IMemberBoxValue = {
+    title: "🖥️ 최종 심사",
+    member: [
+      {
+        name: "조수현",
+        schoolNumber: "2301",
+        phoneNumber: "010-1234-4567",
+        introduce: "열심히 하겠습니다.",
+      },
+    ],
+  };
   return (
     <S.ManagerMainContainer>
       <Sidebar />
       <S.ContentsBox>
         <Title />
         <S.MemberContentsContainer>
-          <MemederBox title="📩 지원 접수"></MemederBox>
-          <MemederBox title="1"></MemederBox>
-          <MemederBox title="2"></MemederBox>
-          <MemederBox title="3"></MemederBox>
-          <MemederBox title="4"></MemederBox>
-          <MemederBox title="5"></MemederBox>
-          <MemederBox title="56"></MemederBox>
+          <MemederBox title={MemberBox1.title} member={MemberBox1.member} />
+          <MemederBox title={MemberBox2.title} member={MemberBox2.member} />
+          <MemederBox title={MemberBox3.title} member={MemberBox3.member} />
         </S.MemberContentsContainer>
       </S.ContentsBox>
     </S.ManagerMainContainer>
