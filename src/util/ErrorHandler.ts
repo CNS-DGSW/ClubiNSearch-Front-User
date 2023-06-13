836ee2046606e8bbeb3b3a8d711ead0f26e3e5e8
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-13 23:44:18 +0900

    ♻️ [Apply] Delete Link Error Checker

--- src/util/ErrorHandler.ts
+++ src/util/ErrorHandler.ts
@@ -6,8 +6,7 @@ const ErrorHandler = (contentValue: IContentsValue) => {
   const regName = /\s/g;
   const regPhone = /^01([0|1|6|7|8|9])([0-9]{3,4})([0-9]{4})$/;
   const regSchoolNumber = /[1-3][1-4][0-2][0-9]/;
-  const regUrl =
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)/;
+
   const initError: IErrorValue = {
     name: {
       context: "",
@@ -22,10 +21,6 @@ const ErrorHandler = (contentValue: IContentsValue) => {
       isError: false,
     },
     introduce: {
-      context: "",
-      isError: false,
-    },
-    link: {
       context: "",
       isError: false,
     },
