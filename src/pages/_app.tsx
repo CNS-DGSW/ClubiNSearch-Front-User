d8c19a66302e21a3720c19de4c80fd5593c8679b
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2023-06-19 13:06:41 +0900

    🐛 : conflict fix
    🐛 : conflict fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/Global.css";
 import Header from "../components/common/Header/Header";
-import Footer from "@/components/common/footer/Footer";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -8,7 +7,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Header />
       <Component {...pageProps} />
-      <Footer />
     </>
   );
 }
