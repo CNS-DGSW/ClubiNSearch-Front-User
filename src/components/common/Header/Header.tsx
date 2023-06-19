483d6320d6809321b60f20c57a23609cc1797916
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-19 23:15:38 +0900

    :recycle: [Header] Fix Header Value

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,27 +1,68 @@
-import headerLogo from '../../../asset/headerLogo.svg';
-import Image from 'next/image';
+import headerLogo from "../../../asset/headerLogo.svg";
+import Image from "next/image";
 import * as S from "./Header.style";
-import Link from 'next/link';
+import Link from "next/link";
+import API from "@/util/api";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const router = useRouter();
+  const [isActive, setIsActive] = useState<boolean>(false);
+  useEffect(() => {
+    const Token: string | null = localStorage.getItem("accessToken");
+    if (Token) {
+      API.get(`api/resume/admin/list/9`, {
+        headers: { Authorization: `Bearer ${Token}` },
+      })
+        .then((e) => {
+          console.log(e);
+
+          setIsActive(true);
+        })
+        .catch((e) => {
+          console.log(e);
+          setIsActive(false);
+        });
+    }
+  }, [router]);
+
+  const LogOutMethod = () => {
+    if (window.confirm("로그아웃 하시겠습니까?")) {
+      setIsActive(false);
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+    }
+  };
   return (
     <header>
       <S.Nav>
         <S.headerLogo>
           <Link href="/">
-            <Image src={headerLogo} alt="logo" width={179} height={35}/>
+            <Image src={headerLogo} alt="logo" width={179} height={35} />
           </Link>
         </S.headerLogo>
-        <S.NavItem>채용 공고</S.NavItem>
-        <S.NavItem>팀 문화</S.NavItem>
-        <S.NavItem>지원서 작성</S.NavItem>
-        <S.NavItem>자주 묻는 질문</S.NavItem>
-        <S.NavItem>
-          <S.StyledLink href="/signin">
-          로그인
-          </S.StyledLink>
-        </S.NavItem>
-        
+        {isActive ? (
+          <>
+            <S.NavItem>
+              <S.StyledLink href="/">채용 공고</S.StyledLink>
+            </S.NavItem>
+            <S.NavItem>지원서 작성</S.NavItem>
+            <S.NavItem>문의 하기</S.NavItem>
+            <S.NavItem>내 정보</S.NavItem>
+            <S.NavItem onClick={LogOutMethod}>로그아웃</S.NavItem>
+          </>
+        ) : (
+          <>
+            <S.NavItem>
+              <S.StyledLink href="/">채용 공고</S.StyledLink>
+            </S.NavItem>
+            <S.NavItem>문의 하기</S.NavItem>
+            <S.NavItem>
+              <S.StyledLink href="/signin">로그인</S.StyledLink>
+            </S.NavItem>
+          </>
+        )}
       </S.Nav>
     </header>
   );
