eed94a806ddc3f56f3d8f87e8078f7445ffab0b1
bae0203 <gmbae06@dgsw.hs.kr>
2023-06-27 08:37:24 +0900

    :recycle 주석삭제

--- src/components/signin/signin.tsx
+++ src/components/signin/signin.tsx
@@ -1,35 +1,34 @@
-import * as S from "./signin.style"
-import TitleBig from "@/asset/TitleBig.svg"
-import Image from "next/image"
-import { useState } from "react"
-import API from "@/util/api"
-import { useNavigate } from "react-router-dom"
-
+import * as S from "./signin.style";
+import TitleBig from "@/asset/TitleBig.svg";
+import Image from "next/image";
+import { useState } from "react";
+import API from "@/util/api";
+// import { useNavigate } from "react-router-dom"
 
 // 로그인
 const signin = () => {
-    const [id,setId] = useState<string>();
-    const [password, setPassword] = useState<string>();
-    const navigate = useNavigate();
+  const [id, setId] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  // const navigate = useNavigate();
 
-    const onSubmit = (e:any) => {
-        e.preventDefault()
-        API.post('/api/auth/login',{
-            accountId: id,
-            password: password
-        })
-        .then((res)=>{
-            // 일단 로컬스토리지에 저장해두는데 좀 더 안전하게 바꿀 것
-            localStorage.setItem('accessToken',res.data.accessToken)
-            localStorage.setItem('refreshToken',res.data.refreshToken)
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    API.post("/api/auth/login", {
+      accountId: id,
+      password: password,
+    })
+      .then((res) => {
+        // 일단 로컬스토리지에 저장해두는데 좀 더 안전하게 바꿀 것
+        localStorage.setItem("accessToken", res.data.accessToken);
+        localStorage.setItem("refreshToken", res.data.refreshToken);
 
-            alert('로그인 성공')
-            navigate('/')
-        })
-        .catch((err)=>{
-            console.error(err)
-        })
-    }
+        alert("로그인 성공");
+        // navigate('/')
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
     <>
@@ -60,12 +59,12 @@ const signin = () => {
             로그인
           </S.SubmitBtn>
 
-                <div>
-                    <S.GrayLink href="/signup">회원가입</S.GrayLink>
-                </div>
-            </S.AllWithoutTitle>
-            </S.ContentWrapper>
-        </>
-    )
-}
-export default signin
+          <div>
+            <S.GrayLink href="/signup">회원가입</S.GrayLink>
+          </div>
+        </S.AllWithoutTitle>
+      </S.ContentWrapper>
+    </>
+  );
+};
+export default signin;
