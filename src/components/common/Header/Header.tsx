f4e5064a8b7bc0b87cfe632189868f971a16cb72
sueeeeh <suehyun51@gmail.com>
2023-06-27 09:56:14 +0900

    :recycle: Admin 코드 없애기

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -8,32 +8,32 @@ import { useRouter } from "next/router";
 
 export default function Header() {
   const router = useRouter();
-  const [isActive, setIsActive] = useState<boolean>(false);
-  useEffect(() => {
-    const Token: string | null = localStorage.getItem("accessToken");
-    if (Token) {
-      API.get(`api/resume/admin/list/9`, {
-        headers: { Authorization: `Bearer ${Token}` },
-      })
-        .then((e) => {
-          console.log(e);
+  // const [isActive, setIsActive] = useState<boolean>(false);
+  // useEffect(() => {
+  //   const Token: string | null = localStorage.getItem("accessToken");
+  //   if (Token) {
+  //     API.get(`api/resume/admin/list/9`, {
+  //       headers: { Authorization: `Bearer ${Token}` },
+  //     })
+  //       .then((e) => {
+  //         console.log(e);
 
-          setIsActive(true);
-        })
-        .catch((e) => {
-          console.log(e);
-          setIsActive(false);
-        });
-    }
-  }, [router]);
+  //         setIsActive(true);
+  //       })
+  //       .catch((e) => {
+  //         console.log(e);
+  //         setIsActive(false);
+  //       });
+  //   }
+  // }, [router]);
 
-  const LogOutMethod = () => {
-    if (window.confirm("로그아웃 하시겠습니까?")) {
-      setIsActive(false);
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
-    }
-  };
+  // const LogOutMethod = () => {
+  //   if (window.confirm("로그아웃 하시겠습니까?")) {
+  //     setIsActive(false);
+  //     localStorage.removeItem("accessToken");
+  //     localStorage.removeItem("refreshToken");
+  //   }
+  // };
   return (
     <header>
       <S.Nav>
@@ -42,27 +42,7 @@ export default function Header() {
             <Image src={headerLogo} alt="logo" width={179} height={35} />
           </Link>
         </S.headerLogo>
-        {isActive ? (
-          <>
-            <S.NavItem>
-              <S.StyledLink href="/">채용 공고</S.StyledLink>
-            </S.NavItem>
-            <S.NavItem>공고 작성</S.NavItem>
-            <S.NavItem>문의 하기</S.NavItem>
-            <S.NavItem>내 정보</S.NavItem>
-            <S.NavItem onClick={LogOutMethod}>로그아웃</S.NavItem>
-          </>
-        ) : (
-          <>
-            <S.NavItem>
-              <S.StyledLink href="/">채용 공고</S.StyledLink>
-            </S.NavItem>
-            <S.NavItem>문의 하기</S.NavItem>
-            <S.NavItem>
-              <S.StyledLink href="/signin">관리자 로그인</S.StyledLink>
-            </S.NavItem>
-          </>
-        )}
+        {}
       </S.Nav>
     </header>
   );
