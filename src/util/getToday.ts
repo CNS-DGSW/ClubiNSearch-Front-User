9d0122a6b4bc4c14602e5ff28318232bfddf51b8
bae0203 <gmbae06@dgsw.hs.kr>
2023-08-16 22:04:33 +0900

    :sparkles: Fix Get today

--- src/util/getToday.ts
+++ src/util/getToday.ts
@@ -1,7 +1,9 @@
 export const getToday = () => {
   const today = new Date();
   const year = today.getFullYear();
-  const month = today.getMonth() + 1;
-  const date = today.getDate();
+  let month = String(today.getMonth() + 1);
+  let date = String(today.getDate());
+  if (String(month).length === 1) month = "0" + String(today.getMonth() + 1);
+  if (String(date).length === 1) date = "0" + String(today.getDate());
   return `${year}-${month}-${date}`;
 };
