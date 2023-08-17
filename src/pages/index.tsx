5b2b2979d4af7e53ee9ea4f59af941a4a4391f0c
bae0203 <gmbae06@dgsw.hs.kr>
2023-08-17 13:47:39 +0900

    :bug: [Main] Fix Build Bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,34 @@
 import Main from "../components/main/main";
-import Image from 'next/image';
-import fs from 'fs';
-import matter from 'gray-matter';
-import Link from 'next/link';
-import { FC, useEffect, useRef, useState } from 'react';
+import Image from "next/image";
+import fs from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
+import { FC, useEffect, useRef, useState } from "react";
 import axios from "axios";
-import API from "@/util/api"
-import {recuitment , EmploymentType} from "@/types/Recuitment"
+import API from "@/util/api";
+import { recuitment } from "@/types/Recuitment";
 
 export async function getStaticProps() {
-  const option = 'api/recruitment/'
-  
-  const {data}:{data:recuitment[]} = await API.get('api/recruitment/');
+  const option = "api/recruitment/";
 
-  const posi = data.map((d : recuitment)=>(
-    d.position
-  ))
+  const { data }: { data: recuitment[] } = await API.get("api/recruitment/");
+
+  const posi = data.map((d: recuitment) => d.position);
 
   return {
-    props : {
-      getposts : data,
-      posi
-    }
-  }
-
+    props: {
+      getposts: data,
+      posi,
+    },
+  };
 }
 
-export default function Home ({getposts, posi} : {getposts: recuitment[] ,posi: string[]} ) {
-
+export default function Home({
+  getposts,
+  posi,
+}: {
+  getposts: recuitment[];
+  posi: string[];
+}) {
   return <Main getposts={getposts} posi={posi} />;
 }
-
