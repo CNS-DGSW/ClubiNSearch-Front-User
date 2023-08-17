aa37a765fb56c1bd4897045539c602ba03b090b5
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2023-08-17 10:28:10 +0900

    ADD : alert pop-up for ask.
    ADD : alert pop-up for ask.

--- src/components/common/Ask/Ask.tsx
+++ src/components/common/Ask/Ask.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 import * as S from "./Ask.style";
 
 export default function Ask() {
+  const askEvent = () => {
+    alert("문의 사항은 카카오워크\n2-3 조수현학생에게 문의 부탁드립니다.");
+  };
   return (
-    <S.Ask>
+    <S.Ask onClick={askEvent}>
       <p>
         채용팀에
         <br />
